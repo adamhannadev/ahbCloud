3bb917cfc692009a7c7ef6f7bd1341d213ee1da6
Adam Hanna <adamhanandev@gmail.com>
2021-04-20 14:22:56 -0700

    Add lessons template

--- client/main.js
+++ client/main.js
@@ -3,6 +3,7 @@ import { Template } from 'meteor/templating';
 import './main.html';
 import './imports/ui/students.html';
 import './imports/ui/teachers.html';
+import './imports/ui/lessons.html';
 
 Template.studentList.helpers({
   students: [
@@ -15,5 +16,14 @@ Template.teacherList.helpers({
   teachers: [
     { lastName: "Hanna", firstName: "Adam" },
     { lastName: "Kottova", firstName: "Tyna" }
+  ]
+});
+
+let today = new Date();
+
+Template.lessonList.helpers({
+  lessons: [
+    { lessonDate: today, lessonTime: today.toTimeString(), student: "Linda", teacher: "Adam" },
+    { lessonDate: today, lessonTime: today.toTimeString(), student: "Liping", teacher: "Adam" }
   ]
 });
