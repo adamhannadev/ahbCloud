d544a3130232f020c7bcdf276a6bce42dced9256
adamhannadev <adamhannadev@gmail.com>
2021-05-06 13:59:49 +0000

    create mockup of calendar week list view

--- client/main.js
+++ client/main.js
@@ -1,9 +1,8 @@
-import { Template } from 'meteor/templating';
-
 import './main.html';
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import '../node_modules/bootstrap/dist/js/bootstrap.js';
 import '../imports/ui/students/students.js';
 import '../imports/ui/teachers.html';
 import '../imports/ui/lessons.js';
-import '../node_modules/bootstrap/dist/css/bootstrap.css';
-import '../node_modules/bootstrap/dist/js/bootstrap.js';
+import '../imports/ui/calendar/calendarList.html';
 
