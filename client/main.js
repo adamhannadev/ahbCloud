a2a666d3320d6feede9e4573b51fe3e1820b7cc0
adamhannadev <adamhannadev@gmail.com>
2021-05-08 14:42:01 +0000

    create teachers form and navbar to main template

--- client/main.js
+++ client/main.js
@@ -1,8 +1,6 @@
-import { Template } from 'meteor/templating';
-
 import './main.html';
 import '../imports/ui/students/students.js';
-import '../imports/ui/teachers.html';
+import '../imports/ui/teachers/teachers.js';
 import '../imports/ui/lessons/lessons.js';
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
 import '../node_modules/bootstrap/dist/js/bootstrap.js';
