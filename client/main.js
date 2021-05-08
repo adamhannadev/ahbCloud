2808c7d9958687a8f67fff0212e86cf5179791a8
adamhannadev <adamhannadev@gmail.com>
2021-05-08 14:11:05 +0000

    refactor lessons templates into own folder and add bootstrap style

--- client/main.js
+++ client/main.js
@@ -3,7 +3,7 @@ import { Template } from 'meteor/templating';
 import './main.html';
 import '../imports/ui/students/students.js';
 import '../imports/ui/teachers.html';
-import '../imports/ui/lessons.js';
+import '../imports/ui/lessons/lessons.js';
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
 import '../node_modules/bootstrap/dist/js/bootstrap.js';
 
