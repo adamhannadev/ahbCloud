4d91cf3d1a489136bf4628a35009fa52d327430f
adamhannadev <adamhannadev@gmail.com>
2021-05-11 21:35:04 +0000

    mock out the layout

--- client/main.js
+++ client/main.js
@@ -3,5 +3,6 @@ import '../imports/ui/navbar/navbar.js';
 import '../imports/ui/students/students.js';
 import '../imports/ui/teachers/teachers.js';
 import '../imports/ui/lessons/lessons.js';
+import '../imports/ui/lessons/calendar.js';
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
 import '../node_modules/bootstrap/dist/js/bootstrap.js';
