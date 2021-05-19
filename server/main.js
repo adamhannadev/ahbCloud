1b6ec6a8a71bd31c9c49fbe13209e23b929764f5
adamhannadev <adamhannadev@gmail.com>
2021-05-19 02:05:56 +0000

    Add init code to populate lessons

--- server/main.js
+++ server/main.js
@@ -1,10 +1,30 @@
 import { Meteor } from 'meteor/meteor';
 
-import '/imports/api/lessons';
+import {Lessons} from '/imports/api/lessons';
 import '/imports/api/students';
 import '/imports/api/teachers';
 import '/imports/api/publications/lessonsPublications';
 
+const insertLessons = lesson => Lessons.insert({
+  lessonDate: lesson.lessonDate,
+  lessonTime: lesson.lessonTime,
+  student: lesson.student,
+  teacher: lesson.teacher
+});
+
 Meteor.startup(() => {
-  // code to run on server at startup
+if (Lessons.find({}).count() === 0){
+  [{
+    lessonDate: new Date(),
+    lessonTime: "6:30pm",
+    student: "Linda",
+    teacher: "Adam"
+  },
+  {
+    lessonDate: new Date(),
+    lessonTime: "7:30pm",
+    student: "Kami",
+    teacher: "Adam"
+  }].forEach(insertLessons);
+}
 });
